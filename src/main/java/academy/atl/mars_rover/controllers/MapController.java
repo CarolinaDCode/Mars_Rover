95b97b4b98c655b55f456e6055a69ce897d9de93
Diana Carhuamanta <dianacarhuamanta03@gmail.com>
2023-09-20 00:26:14 -0500

    add get

--- src/main/java/academy/atl/mars_rover/controllers/MapController.java
+++ src/main/java/academy/atl/mars_rover/controllers/MapController.java
@@ -1,21 +1,33 @@
 package academy.atl.mars_rover.controllers;
 
+import academy.atl.mars_rover.Services.MapService;
+import academy.atl.mars_rover.models.MapEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.NoSuchElementException;
 
-//@CrossOrigin(origins = "*") //para cargar los datos de la bd en el front
-
-//@RequestMapping("map")
-//@RequiredArgsConstructor
+@CrossOrigin(origins = "*") //para cargar los datos de la bd en el front
 @RestController
 @RequestMapping("/map")
 public class MapController {
 
-    //@Autowired
-    //MapRepo mapRepo;
+    private final MapService mapService;
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello dev Holaas";
+    @Autowired
+    public MapController(MapService mapService){
+        this.mapService = mapService;
+    }
+
+    @GetMapping("/coordinates")
+    public ResponseEntity<List<MapEntity>> getAllCoord() {
+        try {
+            List<MapEntity> getAllCoord = mapService.getAllCoord();
+            return ResponseEntity.ok(getAllCoord);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     //@Autowired
